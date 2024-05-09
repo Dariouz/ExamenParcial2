9b1c10cd1704315fcd8b19000d0d609f32aedc44
f208 <f208@STCF208019EF>
2024-05-08 19:03:59 -0600

    Agregar archivos de proyecto.

---
+++ ExamenParcial2/ProductDB.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExamenParcial2
+{
+    public class ProductDB : DbContext
+    {
+        public ProductDB(DbContextOptions<ProductDB> options) : base(options) { }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
